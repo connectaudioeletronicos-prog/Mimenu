4c072f65478b3e64f58de05c849c40b873d55895
connectaudioeletronicos-prog <contatoconnectaudioeletronicos@gmail.com>
2026-07-18 06:36:14 -0300

    Update painel.js

--- backend/src/routes/painel.js
+++ backend/src/routes/painel.js
@@ -6,6 +6,7 @@ const router = express.Router();
 const rateLimit = require('express-rate-limit');
 
 const painelController = require('../controllers/painelController');
+const comunicacaoController = require('../controllers/comunicacaoController');
 
 const limitador = rateLimit({
   windowMs: 15 * 60 * 1000,
@@ -16,5 +17,8 @@ const limitador = rateLimit({
 router.get('/estabelecimentos', limitador, painelController.listarEstabelecimentos);
 router.put('/estabelecimentos/:id/status', limitador, painelController.alternarStatusEstabelecimento);
 router.put('/convites/:id/cancelar', limitador, painelController.cancelarConvite);
+
+router.get('/contatos', limitador, comunicacaoController.listarContatos);
+router.post('/contatos/email', limitador, comunicacaoController.enviarEmail);
 
 module.exports = router;
