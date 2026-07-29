fde41eea842fc0f6bcc9cde5c4982fc156f89157
connectaudioeletronicos-prog <contatoconnectaudioeletronicos@gmail.com>
2026-07-29 15:25:46 -0300

    Delete backend/carrosseis_posicao_ampliada.sql

--- backend/carrosseis_posicao_ampliada.sql
+++
@@ -1,4 +0,0 @@
--- Permite posicionar carrosseis logo apos uma categoria especifica
--- (formato "apos-categoria:<uuid>"), alem dos pontos fixos de sempre.
-ALTER TABLE carrosseis ALTER COLUMN posicao TYPE VARCHAR(80);
-ALTER TABLE carrosseis DROP CONSTRAINT IF EXISTS carrosseis_posicao_check;
