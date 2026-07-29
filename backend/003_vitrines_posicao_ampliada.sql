7d7c47a64c75d49d806a0e33a2f299f0e27e03ea
connectaudioeletronicos-prog <contatoconnectaudioeletronicos@gmail.com>
2026-07-29 15:26:26 -0300

    Delete backend/003_vitrines_posicao_ampliada.sql

--- backend/003_vitrines_posicao_ampliada.sql
+++
@@ -1,3 +0,0 @@
--- Mesmo motivo da migration 002, aplicado a vitrines.
-ALTER TABLE vitrines ALTER COLUMN posicao TYPE VARCHAR(80);
-ALTER TABLE vitrines DROP CONSTRAINT IF EXISTS vitrines_posicao_check;
