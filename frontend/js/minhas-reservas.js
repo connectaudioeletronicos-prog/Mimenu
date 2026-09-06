7abe85b877a6e710a6d0b7bec804f1da18d9cd74
connectaudioeletronicos-prog <contatoconnectaudioeletronicos@gmail.com>
2026-09-06 04:44:12 -0300

    Update navigation badge update function for reservations

--- frontend/js/minhas-reservas.js
+++ frontend/js/minhas-reservas.js
@@ -43,7 +43,7 @@ async function iniciarMinhasReservas() {
 
   preencherSaudacaoConta(conta);
   configurarNavegacaoConta('reservas');
-  atualizarBadgeAbaNotificacoes();
+  atualizarBadgesNavegacaoConta('reservas');
   await carregarMinhasReservas();
 
   document.getElementById('tela-carregando').classList.add('oculto');
